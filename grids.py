9935a742d3b55a1b744187a3c4cd37ad05919042
p3dr1np3trus <p3dr1n4321@gmail.com>
2024-12-04 18:27:06 -0300

    Update grids.py

--- grids.py
+++ grids.py
@@ -1,36 +1,28 @@
-class AxisEnd:
-    def __init__(self):
-        self.property = "AxisEnd"
-
 class Grid2d:
     def __init__(self, xSize: int, items: list):
         self.Xsize = xSize
         self.items = items
+
         if len(self.items) % self.Xsize != 0:
             self.Xsize = len(self.items)
 
+        self.content = [
+            self.items[i:i + self.Xsize] for i in range(0, len(self.items), self.Xsize)
+        ]
+
+
     def printDebug(self):
-        printcontent = []
-        iterator = 0
-        for item in self.items:
-            printcontent.append(item)
-            iterator += 1
-            if len(printcontent) == self.Xsize:
-                print(printcontent)
-                printcontent = []
+        for item in self.content:
+            print(item)
     
-    def iterate(self):
-        iterated = []
-        for item in self.items:
-            iterated.append(item)
-            if len(iterated) % self.Xsize == 0:
-                iterated.append(AxisEnd())
-        return iterated
-    
+    def getItem(self, x, y):
+        return self.content[y][x]
+
+
 class Grid3d:
-    def __init__(self, xSize: int, Zsize: int, items: list):
+    def __init__(self, xSize: int, Ysize: int, items: list):
         self.Xsize = xSize
-        self.Zsize = Zsize
+        self.Zsize = Ysize
         self.items = items
         if len(self.items) % self.Xsize != 0:
             self.Xsize = len(self.items)
@@ -39,23 +31,34 @@ class Grid3d:
             self.Zsize = len(self.items)
             self.Xsize = 1
 
+
+        self.content = []
+        iterator = 0
+
+        for z in range(Ysize):
+            layer = []
+            for x in range(xSize):  
+                row = items[iterator:iterator + xSize] 
+                layer.append(row)
+                iterator += xSize
+            self.content.append(layer)
+
     def printDebug(self):
-        printcontent = []
-        xprint = []
-        for item in self.items:   
-            xprint.append(item)
-            if len(xprint) == self.Xsize:
-                printcontent.append(xprint)
-                xprint = []
-                if len(printcontent) == self.Zsize:
-                    print(printcontent)
-                    printcontent = []
+        for row in self.content:
+            print(row)
 
                             
-
+    def getItem(self, row, col, layer):
+        return self.content[col][layer][row]
+        
 
 def examplefunc():
     print("Grid2d:")
     Grid2d(3, ["a", "b", "c", "d", "e", "f", "g", "h", "i"]).printDebug()
     print("Grid3d:")
     Grid3d(2, 3, ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]).printDebug()
+
+
+examplefunc()
+print(Grid2d(3, ["a", "b", "c", "d", "e", "f", "g", "h", "i"]).getItem(2, 2))
+print(Grid3d(2, 6, ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]).getItem(2, 2, 2))
